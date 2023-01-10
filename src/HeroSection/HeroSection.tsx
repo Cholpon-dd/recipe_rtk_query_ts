1c5293ea68d8cd444f6089b9fb0d540214ac7db9
Cholpon <cholpon.damirbekova@gmail.cmom>
2023-01-11 00:00:37 +0600

    feat: change hero section image

--- src/HeroSection/HeroSection.tsx
+++ src/HeroSection/HeroSection.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Search from "../components/Search/Search";
+// import main from '../assets/image/main.jpg'
 
 const HeroSection = () => {
     return (
-        <section className="hero__wrapper">
+        <section className="hero__wrapper" style={{
+            backgroundImage:'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(../../public/main.jpg)',
+            backgroundSize:'cover',
+            backgroundRepeat:'no-repeat',
+            backgroundPosition:'center',
+
+        }}>
             <div className="hero__container">
                 <h1 className="hero__title">What are you cooking today?</h1>
                 <h2 className="hero__sub">Easy way to make your favourite dish</h2>
